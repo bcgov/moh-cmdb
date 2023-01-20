63f3fe3c7597a96d756f8f5ecda02debc1083295
Raili <100877693+Raifry@users.noreply.github.com>
2023-01-20 14:18:07 -0800

    Add files via upload

---
+++ Production Configuration/conf/setenv.sh
@@ -0,0 +1,4 @@
+JAVA_OPTS="-Djavax.net.ssl.trustStore=/data/gfadmin/software/tomcat/conf/permanent.jks $JAVA_OPTS"
+JAVA_OPTS="-Djdk.tls.trustNameService=true $JAVA_OPTS"
+JAVA_OPTS="-Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true $JAVA_OPTS "
+
