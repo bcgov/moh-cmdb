b6c11d0b72ff1376d25f0657b6aa67882395e1ed
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2023-11-21 08:51:07 -0800

    Update secretmanager.tf

--- Infrastructure/secretmanager.tf
+++ Infrastructure/secretmanager.tf
@@ -2,6 +2,15 @@ resource "aws_secretsmanager_secret" "pg_user" {
   name = "PG_USER"
 }
 
+resource "aws_secretsmanager_secret" "pg_url" {
+  name = "PG_URL"
+}
+
+resource "aws_secretsmanager_secret_version" "pg_url" {
+  secret_id     = aws_secretsmanager_secret.pg_url.id
+  secret_string = "changeme"
+}
+
 resource "aws_secretsmanager_secret_version" "rds_credentials" {
   secret_id     = aws_secretsmanager_secret.pg_user.id
   secret_string = <<EOF
